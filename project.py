494ba167abf51670adebbdc3aabc66a7caa8727b
Anson <anson32330200@gmail.com>
2016-11-15 22:48:25 -0800

    Change login_session to session

--- project.py
+++ project.py
@@ -310,7 +310,7 @@ def showRestaurants():
     restaurants = session.query(Restaurant).order_by(asc(Restaurant.name))
     if 'username' not in login_session:
         return render_template('publicrestaurants.html', restaurants=restaurants)
-    return render_template('restaurants.html', restaurants=restaurants, login_session=login_session)
+    return render_template('restaurants.html', restaurants=restaurants)
 
 
 # Create a new restaurant
